856c987a52d749e371913962f3586d0f803a08ec
LuqmanAristio <96041357+LuqmanAristio@users.noreply.github.com>
2022-06-04 21:05:51 +0700

    Add files via upload

---
+++ laundry/paket/paket.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Paket</title>
+	<link rel="stylesheet" type="text/css" href="../css/style-dashboard.css">
+</head>
+<body>
+
+	<div class="header">
+		<h1>GO LAUNDRY</h1>
+	</div>
+
+	<div class="sidebar">
+		<nav>
+			<ul>
+				<li style="margin-top: 50px;"><a href="../dashboard.php">Dashboard</a></li>
+				<li><a href="../pegawai/pegawai.php">User</a></li>
+				<li><a href="../pelanggan/pelanggan.php">Pelanggan</a></li>
+				<li><a href="paket.php">Paket</a></li>
+				<li><a href="../transaksi/transaksi.php">Transaksi</a></li>
+			</ul>	
+		</nav>
+	</div>
+
+	<div class="main">
+		<h1>Paket</h1>
+
+
+	</div>
+
+</body>
+</html>
